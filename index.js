5544eab94351936da0191aa8f129052f58442a41
Gábor Áron Zsolt <aron_zsolt_gabor@epam.com>
2016-09-05 17:05:44 +0200

    getting closer

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
+const os = require('os')
+const env = Object.assign({}, process.env)
+const DEBUG = env.DEBUG || true
 const wdb = require('win-detect-browsers')
 const spawn = require('child_process').spawn
 // const start = through2.obj(function(b, _, next){console.log(b); next(); })
 
 function start(executable, opts, cb){
-    const env = Object.assign({}, process.env)
     if (opts.display) env.DISPLAY = `:${opts.display}`
 
     const dataDir = opts.dataDir ||
-          `/tmp/${Math.random().toString(16).slice(2)}`
-    const args = [
-        opts.proxy && `--proxy-server=${opts.proxy}`,
-        `--user-data-dir=${dataDir}`,
-          '--disable-restore-session-state',
-          '--no-default-browser-check',
-          '--start-maximized',
-          '--disable-default-apps',
-          '--disable-sync',
-          '--enable-fixed-layout',
-          '--no-first-run',
-          '--noerrdialogs',
-          opts.uri
-    ].filter(Boolean)
+          `${os.tmpdir()}\\${Math.random().toString(16).slice(2)}`
+
+    const args = [ opts.proxy && `--proxy-server=${opts.proxy}`
+                 , `--user-data-dir=${dataDir}`
+                 , '--disable-restore-session-state'
+                 , '--no-default-browser-check'
+                 , '--start-maximized'
+                 , '--disable-default-apps'
+                 , '--disable-sync'
+                 , '--enable-fixed-layout'
+                 , '--no-first-run'
+                 , '--noerrdialogs'
+                 , opts.uri
+                 ].filter(Boolean)
 
     cb(spawn(executable, args, { env }))
 }
@@ -31,10 +33,11 @@ function test(opts, cb){
         if ( Array.isArray(found) && found.length > 0 ) {
             found.some(function(f){
                 try {
-                    console.log('trying path:', f.path)
+                    if ( DEBUG ) console.log('trying path:', f.path)
                     start(f.path, opts, cb)
+                    return true
                 } catch (e) {
-                    console.error('failed: ', f.path)
+                    if ( DEBUG ) console.error('failed: ', f.path)
                     return false
                 }
             })
--- test.js
+++ test.js
@@ -1,11 +1,34 @@
 const test = require('tape')
 const wc = require('./index.js')
 const http = require('http')
+const fs = require('fs')
+const content = fs.readFileSync('./content.html')
+const csp = require('js-csp')
+const {go, chan, put, putAsync, takeAsync} = csp
+const ch = chan()
 
-test('run local file', function(t){
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.end(content)
+})
+
+server.listen(0, 'localhost', () => {
+    putAsync(ch, 'listening')
+})
+
+test('local static server', function(t){
     t.plan(1)
-    t.equal(-0, +0)
-    wc({}, function(){
-        console.log(arguments)
+    takeAsync(ch, (str) => {
+        const port = server.address().port
+        wc({uri: 'http://localhost:'+ port }, function(ps){
+            ps.stderr.setEncoding('utf8')
+            ps.stderr.on('data', (data) => {
+                console.log('err', data);
+            });
+            t.ok('request received')
+            server.close()
+            ps.kill()
+            t.end()
+        })
     })
 })
